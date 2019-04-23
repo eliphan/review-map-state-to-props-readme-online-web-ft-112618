ce25331a0f422fa7875c5135a7365c7979894273
Elise Phan <aliceconnic@gmail.com>
2019-04-23 23:03:46 +0000

    Automatically backed up by Learn

--- src/App.js
+++ src/App.js
@@ -34,5 +34,7 @@ class App extends Component {
 }
 
 const mapStateToProps = (state) => {
-  return { orangePeel: ['a', 'b', 'c'] };
-};
+  return { items: state.items }
+}
+ 
+export default connect(mapStateToProps)(App);
